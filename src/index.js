1166c4a14f99a1eb3a3ebd1aca9ad4dbff0d5e75
Oskar-Gudmundur-Kristinsson <ogk2@hi.is>
2018-11-29 23:01:07 +0000

    almost done

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ import List from './lib/list';
 
 document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('body');
-  const isLecturePage = page.classList.contains('lecture-page');
+//  const isLecturePage = page.classList.contains('');
 
-  if (isLecturePage) {
-
-  } else {
+  try{
+    const isLecturePage = document.querySelector('lecture-page');
+    console.log('hi');
+  }
+  catch(err){
     const list = new List();
     list.load();
   }
+});
+
+var elementsArray = document.querySelectorAll('.section');
+var lesson = document.querySelector('.section').childNodes;// = document.querySelectorAll('.section');
+console.log(lesson);
+
+elementsArray.forEach(function(elem) {
+    elem.addEventListener("click", function(e) {
+      e.target.preventDefault;
+      const temp = window.location.href;
+      window.location.href = temp+"../fyrirlestur.html";
+      const temp1 = e.target.childNodes;
+      console.log(e.target.childNodes);
+      lesson = e.target.querySelector('.section__title');
+      console.log(lesson);
+    });
 });
--- src/lib/list.js
+++ src/lib/list.js
@@ -1,8 +1,18 @@
 import { empty } from './helpers';
 
 export default class List {
+  // eslint-disable-next-line
+   // eslint-disable-line
+
   constructor() {
     this.container = document.querySelector('.list');
+  }
+
+  populate() {
+    const son = JSON.parse('lectures.json');
+    const vid = document.createElement('iframe');
+    vid.src = son.lectures[0].content.data;
+    vid.appendChild(this.container);
   }
 
   load() {
